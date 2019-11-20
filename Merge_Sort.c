d03510217dea7fbe6205ffb2b25fd30ab3790a1e
asif <ahamedasif4444@gmail.com>
2019-11-20 09:02:30 +0530

    mergesort

---
+++ Merge_Sort.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+
+//Prototypes
+void merge_sort(int *a, int first, int last);
+void merge(int *a, int first, int mid, int last);
+void print(int *a, int n);
+
+//Main Function
+int main()
+{
+
+int i, j, first, last, n;
+
+printf("Enter the value of n: \n");
+scanf("%d", &n);
+
+int a[n];
+
+printf("Enter the Elements of n: \n");
+for (i = 0; i < n; i++)
+{
+scanf("%d", &a[i]);
+}
+
+merge_sort(a, 0, n - 1);
+print(a, n);
+}
+
+void merge_sort(int *a, int first, int last)
+{
+if (first < last)
+{
+int mid = (first + last) >> 1;
+merge_sort(a, first, mid);
+merge_sort(a, mid + 1, last);
+merge(a, first, mid, last);
+}
+}
+
+void merge(int *a, int first, int mid, int last)
+{
+int l_size = mid - first + 1;
+int r_size = last - mid;
+int LA[l_size];
+int RA[r_size];
+int i, j;
+int k = first;
+
+for (i = 0; i < l_size; i++)
+{
+LA[i] = a[first + i];
+}
+
+for (j = 0; j < r_size; j++)
+{
+RA[j] = a[mid + 1 + j];
+}
+i = 0;
+j = 0;
+k = first;
+
+while (i < l_size && j < r_size)
+{
+
+if (LA[i] < RA[j])
+{
+a[k++] = LA[i++];
+}
+else
+{
+a[k++] = RA[j++];
+}
+}
+//Copy Remaining bytes
+while(i < l_size)
+{
+a[k++] = LA[i++];
+}
+while(j < r_size)
+{
+a[k++] = RA[j++];
+}
+}
+//Print
+void print(int *a, int n)
+{
+int i = 0;
+while (i < n)
+{
+printf("%d--", a[i]);
+i++;
+}
+}
